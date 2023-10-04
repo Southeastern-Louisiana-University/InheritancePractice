500fa05959e65ec15c8ebcdc47e2eea6ec690364
Steele Russell <sarxyz@users.noreply.github.com>
2023-10-03 21:40:17 -0500

    Initial commit

---
+++ src/TestPerson.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class TestPerson {
+
+  @Test
+  @DisplayName("Create default person")
+  public void testPersonDefault()
+  {
+    Person p = new Person();
+    assertEquals("", p.toString());
+    assertEquals("", p.getName());
+  }
+  @Test
+  @DisplayName("Create person with name")
+  public void testPersonWithName()
+  {
+    Person p = new Person("Harry Potter");
+    assertEquals("Harry Potter", p.toString());
+    assertEquals("Harry Potter", p.getName());
+  }
+  @Test
+  @DisplayName("Set name")
+  public void testPersonSetName()
+  {
+    Person p = new Person();
+    p.setName("Harry Potter");
+    assertEquals("Harry Potter", p.toString());
+    assertEquals("Harry Potter", p.getName());
+  }
+  @Test
+  @DisplayName("Create person with name")
+  public void testPersonEquals()
+  {
+    Person p1 = new Person("Harry Potter");
+    Person p2 = new Person("Harry Potter");
+    Person p3 = new Person("Sirius Black");
+    assertTrue(p1.equals(p2));
+    assertFalse(p1.equals(p3));
+    assertFalse(p2.equals(p3));
+  }
+}
